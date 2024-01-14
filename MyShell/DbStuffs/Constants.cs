ae0df7c5b7d802094508516f418da49a3929c03a
SnailCape <koopacapey@gmail.com>
2024-01-14 19:53:13 +0100

    Database Added database folder and created it

---
+++ MyShell/DbStuffs/Constants.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace MyShell.DbStuffs
+{
+    public static class Constants
+    {
+        private const string DBFilename = "MyShellSQLite.db";
+
+        public const SQLiteOpenFlags flags =
+            SQLiteOpenFlags.ReadWrite |
+            SQLiteOpenFlags.Create |
+            SQLiteOpenFlags.SharedCache;
+
+        public static string DatabasePath
+        {
+            get
+            {
+                return Path.Combine(FileSystem.AppDataDirectory, DBFilename);
+            }
+        }
+    }
+}
