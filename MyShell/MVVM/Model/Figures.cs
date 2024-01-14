fb2990866317b29b7ed53648e6d730a9150de81c
SnailCape <koopacapey@gmail.com>
2024-01-14 20:42:51 +0100

    Crash Doign a quick commit because VSC crashed and had to recover. Don't want to go through that stress again

---
+++ MyShell/MVVM/Model/Figures.cs
@@ -0,0 +1,33 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShell.MVVM.Model
+{
+    class Figures
+    {
+        [Table("FigureData")]
+        public class Figure
+        {
+            [PrimaryKey, AutoIncrement] public int Id { get; set; }
+
+
+            [Column("figurename"), Indexed, NotNull] public string? Name { get; set; } [Unique]
+
+            [Column("price"), Indexed, NotNull] public int? Price { get; set; }
+
+            [Column("brand"), Indexed, NotNull] public string? Brand { get; set; }
+
+            [Column("releasedate"), Indexed, NotNull] public string? ReleaseDate {get; set;}
+
+            [Column("series"), Indexed, NotNull] public string? Series {get; set;}
+
+            
+
+            
+        };
+    }
+}
